733d24dc561fcc59c6b20f49728d3d4a3039ecfa
James Williams <james.williams@computerminds.co.uk>
2018-11-19 14:29:01 +0000

    Fork of pauldotknopf/docgen#archived/attempt3, moved to root dir.

---
+++ fragments.js
@@ -0,0 +1,14 @@
+import { graphql } from "gatsby"
+
+export const pageTreeFragement = graphql`
+  fragment pageTree on Query {
+    currentPage: allSitePage(filter: {path: {eq:$slug}}) {
+        edges {
+          node {
+            menu
+            path
+          }
+        }
+      }
+  }
+`
