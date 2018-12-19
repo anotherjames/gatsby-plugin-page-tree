afec79439521c248c4b9b689a5bfdb4c2a873dec
Stephen Tweeddale <stephen.tweeddale@computerminds.co.uk>
2018-12-19 09:52:08 +0000

    Don't filter out inactive links from the tree

--- menu-builder.js
+++ menu-builder.js
@@ -78,23 +78,6 @@ function buildMenuFromNodes(nodes, selectedPath, ignorePaths) {
     for (let item of result) {
         item.children = sortChildren(item.children);
     }
-    // Now that we built the entire tree, let's remove the nodes that don't matter.
-    const collapseChildren = (item) => {
-        // We only want to consider collapsing children if this current node can be selected.
-        // If it isn't a valid page, then the user can't navigate to it to see it's children.
-        if (!item.isEmptyParent) {
-            // Collapse children if this node isn't in the active selected path.
-            if (!item.active && !item.selected) {
-                item.children = [];
-            }
-        }
-        for (let child of item.children) {
-            collapseChildren(child);
-        }
-    };
-    for (let child of result) {
-        collapseChildren(child);
-    }
     return result;
 }
 exports.buildMenuFromNodes = buildMenuFromNodes;
